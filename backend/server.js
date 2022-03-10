b50ca3ad6e0267836284fa3480f6d788f3536a40
Rajeevalochan R <rajeev@innocrux.com>
2022-03-10 22:40:00 +0530

    Added DB config and goal models

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const colors = require('colors');
 const { errorHandlerMiddleware } = require('./middleware/errorMiddleware');
+const { connectDB } = require('./config/db');
 const dotenv = require('dotenv').config();
 const PORT = process.env.NODE_PORT || 5000;
 
+connectDB();
 const app = express();
 
 app.use(express.json());
